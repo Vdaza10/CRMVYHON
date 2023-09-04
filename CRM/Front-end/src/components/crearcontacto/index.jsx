544eb0c2a4e27f058afabc91f7b95d768fe0f097
oscar gomez <Ogomez@ulearning.com.co>
2023-09-04 14:29:05 -0500

    conexiones casi listas

--- CRM/Front-end/src/components/crearcontacto/index.jsx
+++ CRM/Front-end/src/components/crearcontacto/index.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
-import { Div1,  Container1, Caja, Parrafo, Img, Parrafo1, Caja1, Input,  Caja2, Boton1, Boton2 , ContactoSelect } from "./styled";
+import React, { useState, useEffect } from "react";
+import { Div1,  Container1, Caja, Parrafo, Img, Parrafo1, Caja1, Input,  Caja2, Boton1, Boton2, Select ,  } from "./styled";
 import imagen from "../img/img_x.webp"
-import axios from "axios";
+import Axios from "axios";
 import Validator  from "validator";
 
 function Retorno4() {
     const[nombreUsuario, setNombreUsuario] = useState("")
     const[cargo, setCargo]= useState ("")
     const[telefono, setTelefono] = useState("")
-    const[email, setEmail]= useState("helena@gmail.com")
+    const[email, setEmail]= useState("")
     const[empresaContacto, setEmpresaContacto] = useState("")
+    const[empresa, setEmpresa] = useState([]);
 
     const validacion = (e) => {
         let emai = e.target.value;
@@ -21,27 +22,43 @@ function Retorno4() {
         }
     };
 
-const datosIngresados = (e) => {
-    e.preventDefault();
-
-    if (nombreUsuario && cargo && telefono && empresaContacto) {
-        axios.post("http://localhost:3005/contacto",{
-            nombreUsuario : nombreUsuario,
-            cargo : cargo,
-            telefono: telefono,
-            email : email,
-            empresaContacto : empresaContacto
-        })
-        .then((response) => {
+    const fetchEmpresa = async () => {
+        try {
+            const response = await Axios.get("http://localhost:3005/company");
+            setEmpresa(response.data);
             console.log(response.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        
-        })}else{
-            alert('ingrese todos los valores')
-        };
+        } catch (error) {
+            console.error("Error al obtener segmentos:", error);
+        }
     };
+
+    useEffect(() => {
+        fetchEmpresa();
+    }, []);
+
+    const createContacto = (e) => {
+        e.preventDefault();
+    
+        if (nombreUsuario && cargo && telefono && email && empresaContacto !== "option") {
+            Axios.post("http://localhost:3005/contacto", {
+                nombreContacto: nombreUsuario,
+                cargo: cargo,
+                telefono: telefono,
+                correo: email,
+                contactoEmpresa: empresaContacto
+            })
+            .then((response) => {
+                console.log(response.data);
+                // Aquí puedes realizar cualquier acción adicional después de guardar
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        } else {
+            alert('Ingrese todos los valores correctamente');
+        }
+    };
+    
 
 
     // Estado para controlar si el componente está cerrado o abierto
@@ -51,11 +68,11 @@ const datosIngresados = (e) => {
     const cerrarcomponente = () => {
         setCerrar(false);
     }
-
     // Si el estado 'cerrar' es falso, devuelve null para ocultar el componente
     if (!cerrar){
         return null 
-    }
+    };
+
     return (
         <Div1 >
             {/* Contenedor principal */}
@@ -70,28 +87,33 @@ const datosIngresados = (e) => {
                 {/* Segunda sección */}
                 <Caja1>
                     <Parrafo1><h3>Nombre</h3></Parrafo1>
-                    <Input placeholder="Ingresar el nombre del contacto" onChange={(event)=>{setNombreUsuario(event.target.value)}} type="text"required></Input>
+                    <Input placeholder="Ingresar el nombre del contacto" onChange={(e)=>{setNombreUsuario(e.target.value)}}></Input>
                     <Parrafo1><h3> Cargo</h3></Parrafo1>
-                    <Input placeholder="Ingresa el contacto del contacto" onChange={(event)=>{setCargo(event.target.value)}} type="text"required></Input>
+                    <Input placeholder="Ingresa el contacto del contacto" onChange={(e)=>{setCargo(e.target.value)}}></Input>
                     <Parrafo1><h3>Telefono</h3></Parrafo1>
-                    <Input placeholder="Ingresar el telefono del contacto"onChange= {(event)=>{setTelefono(event.target.value)}} type="number"required></Input>
+                    <Input placeholder="Ingresar el telefono del contacto"onChange= {(e)=>{setTelefono(e.target.value)}}></Input>
                     <Parrafo1><h3>Email</h3></Parrafo1>
                     <Input placeholder="Ingresar el email del contacto" onChange={validacion}  type = "email"
                     ></Input>
-                    <Parrafo1 onChange= {(event)=>{setEmpresaContacto(event.target.value)}}><h3>Empresa del contacto </h3></Parrafo1>
-                    <ContactoSelect></ContactoSelect>
-                    {/* <Parrafo2><h3>INFORMACION ADICIONAL</h3></Parrafo2> */}
+                    <Parrafo1><h3>Empresa del contacto </h3></Parrafo1>
+                    <Select value={empresaContacto} onChange={(e) => {setEmpresaContacto(e.target.value)}}>
+                        <option value="option"></option>
+                        {empresa.map(empresa => (
+                            <option key={empresa.idEmpresa} value={empresa.idEmpresa}>
+                                {empresa.nombreEmpresa}
+                            </option>
+                        ))}
+                    </Select>
                 </Caja1>
                 <hr />
                  {/* Tercera sección */}
                 <Caja2>
                     {/* Botones para cancelar o crear el contacto */}
                     <Boton2>Cancelar</Boton2>
-                    <Boton1 onClick ={datosIngresados}>Crear contacto</Boton1>
+                    <Boton1 onClick={createContacto}>Crear contacto</Boton1>
                 </Caja2>
             </Container1>
         </Div1>
-
     );
     }
 export default Retorno4;
