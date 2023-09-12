1b72c17de5dfa2263e830f1c80ce9a754e4471ac
Helena De Alba <hdealba@ulearning.com.co>
2023-09-12 16:56:51 -0500

    carruzel de tarjetas

--- CRM/Front-end/src/components/vistaPrincipal/principal.jsx
+++ CRM/Front-end/src/components/vistaPrincipal/principal.jsx
@@ -1,51 +1,62 @@
-import { ConInformacion, Contenedor, ContenedorCarruzel, ContenedorTarjeta, Tarjeta2 } from "./styled";
-import Menu from "../menu/principal"
-import { useState } from "react"
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-import {Carousel} from "react-responsive-carousel"
+import React, { useState } from "react";
+import {Contenedor,ContenedorTarjeta,Tarjeta1,Tarjeta2,} from "./styled";
+import Menu from "../menu/principal";
 
 const Vistaprincipal = () => {
-    // Datos de las tarjetas a mostrar en el carrusel
-    const tarjetas = [
-        {titulo: "Negocios", informacion: "informacion de negocio"},
-        {titulo: "Empresas", informacion: "informacion de empresa"},
-        {titulo: "Contactos", informacion: "informacion de contactos"},
-        {titulo: "Tareas",informacion: "informacion de tareas"},
-        {titulo: "Analisis", informacion: "informacion de analisis"},
-        {titulo: "Markenting", informacion: "informacion de markenting"},
-    ];
-     // Estado para almacenar la tarjeta actualmente visible en el carrusel
-    const [informacion, setInformacion] = useState(tarjetas[2]);
-    // Función para mostrar una tarjeta específica en el carrusel
-    const mostar = (index) => {
-        setInformacion(tarjetas[index])
+const [tarjetaActual, setTarjetaActual] = useState(0);
+
+// Define an array of card content (you can replace this with your own data)
+const tarjetas = [
+    { title: "EMPRESA", description: "This is card A." },
+    { title: "Contactos", description: "This is card B." },
+    { title: "Negocios", description: "This is card C." },
+    { title: "Tareas", description: "This is card D." },
+    { title: "Markenting", description: "This is card E." },
+];
+
+const asignarAnimacion = (claseActual, claseSiguiente) => {
+    const tarjetas = document.querySelectorAll(".tarjeta");
+
+    tarjetas.forEach((tarjeta) => {
+    if (tarjeta.classList.contains(claseActual)) {
+        tarjeta.classList.remove(claseActual);
+        tarjeta.classList.add(claseSiguiente);
     }
-    return(
-        <>
-        <Menu/>
-        <Contenedor >
-            <ContenedorTarjeta>
-                <Carousel showThumbs={false} showStatus={false} selectedItem={tarjetas.indexOf(informacion)} onChange={mostar} autoPlay interval={3000} stopOnHover={true} axis="horizontal" infiniteLoop = {true}
-                style={{ display: "flex", flexDirection: "column", maxWidth: "100%" }}>
-                {/* Mapeo de las tarjetas para crear elementos en el carrusel */}
-                {tarjetas.map((tarjetas)=>{
-                    return(
-                        <Tarjeta2 key={informacion}>  
-                        <ContenedorCarruzel>
-                            <h1>{tarjetas.titulo}</h1>
-                            <p>{tarjetas.informacion}</p>
-                            <p>aqui en este contendor va la informacion</p>
-                        </ContenedorCarruzel>
-                        </Tarjeta2>
-                    )})}
-                </Carousel>
-                
-            </ContenedorTarjeta>
-            {/* Contenedor para la información adicional */}
-            <ConInformacion>
-            </ConInformacion>
-        </Contenedor>
-        </>
-    )
-}
+    });
+};
+
+const handleClickNext = () => {
+    const nuevaTarjeta = (tarjetaActual + 1) % tarjetas.length;
+    asignarAnimacion(tarjetas[tarjetaActual], tarjetas[nuevaTarjeta]);
+    setTarjetaActual(nuevaTarjeta);
+};
+
+setTimeout(() => {
+    handleClickNext();
+}, 2000);
+
+return (
+    <>
+    <Menu />
+    <Contenedor className="container">
+        <ContenedorTarjeta>
+        <Tarjeta2 className="carousel">
+            {tarjetas.map((tarjeta, index) => (
+            <Tarjeta1
+                key={index}
+                className={`tarjeta ${
+                index === tarjetaActual ? "active" : ""
+                }`}
+            >
+                <h2>{tarjeta.title}</h2>
+                <p>{tarjeta.description}</p>
+            </Tarjeta1>
+            ))}
+        </Tarjeta2>
+        </ContenedorTarjeta>
+    </Contenedor>
+    </>
+);
+};
+
 export default Vistaprincipal;
--- CRM/Front-end/src/components/vistaPrincipal/styled.jsx
+++ CRM/Front-end/src/components/vistaPrincipal/styled.jsx
@@ -1,68 +1,48 @@
 import styled from "styled-components";
 
-// Contenedor principal
 export const Contenedor = styled.div`
-    height:100%;
-    width: 100%;
-    /* background-color: pink;  */
-    position: absolute;
-`
-// Contenedor para el carrusel de tarjetas
-export const ContenedorTarjeta = styled.div`
-    height: 75vh;
-    width: 100%; 
-    margin-top: 0.0%;
-    /* background: radial-gradient(90deg, #a9c8eb 10%, rgba(0,212,255,1) 23%, #a9c8eb 100%); */
-    /* background-color: #8eb0d3;  */
-    display: flex;
-    align-items: center;
-    left: auto; 
-    justify-content: space-between;
-    justify-content: space-evenly;
-    @media screen and (max-width: 700px) {  
-    }
-`
-    export const ContenedorCarruzel = styled.div`
-    background-color: #e494ec;
-    height: 30em;
-    width: 15em;
-    `
-// Tarjeta individual (ejemplo, puedes tener múltiples tarjetas con nombres diferentes)
-    export const Tarjeta1 = styled.div`
-        height: 90%;
-        width: 100%; 
-        background-color: pink;
-        align-items: center;
-        display: flex; 
-    `
-// Tarjeta utilizada en el carrusel
-    export const Tarjeta2 = styled.div`
-        height: 65vh;
-        width: 100%; 
-        /* background-color: blue;  */
-        display: flex;
-        justify-content: center;
-        /* align-items: center; */
-        /* animation: rotateCard 3s linear infinite; */
-        @keyframes rotateCard {
-        0% {
-        transform: rotateY(0deg);
-        }
-        100% {
-        transform: rotateY(360deg);
-        }
-    }
-    `
-// Contenedor para la información adicional (fuera del carrusel)
-    export const ConInformacion = styled.div`
-        width: 100%;
-        height: 60vh;
-        `
-// Ejemplo de estilo para un título (puedes personalizarlo según tus necesidades)
-    export const Titulo = styled.h1`
-    font-size: 24px;
-    font-weight: bold;
-    color: #333; 
-    `
+/* background-color: blueviolet; */
+height: 750px;
+display: grid;
+grid-gap: 10px;
+grid-template: 1fr / 5% 1fr 5%;
+grid-template-areas: "prev carousel next";
+justify-items: center;
+align-items: center;
+margin-top: 7vh;
+`;
 
+export const ContenedorTarjeta = styled.div`
+/* background-color: #0000ff; */
+display: flex;
+justify-content: center;
+align-items: center;
+height: 700px;
+width: 234em;
+`;
+
+export const Tarjeta1 = styled.div`
+width: 320px;
+height: 600px;
+background-color: #ff0000;
+transition: all 0.3s ease;
+opacity: 0;
+transform: translateX(20px);
+
+&.active {
+    opacity: 1;
+    transform: translateX(0);
+}
+`;
+
+export const Tarjeta2 = styled.div`
+    display: flex;
+    background-color: silver;
+    height: 600px;
+    margin-left: 1820%;
+`;
+
+// export const ConInformacion = styled.div``;
+
+// export const Titulo = styled.h1``;
 
