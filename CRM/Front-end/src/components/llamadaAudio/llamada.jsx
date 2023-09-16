04cc69248dfdb536eb4be21f7a44b40a761e782e
Vdaza10 <vdaza@ulearning.com.co>
2023-09-16 18:04:42 -0500

    archivos temporal

---
+++ CRM/Front-end/src/components/llamadaAudio/llamada.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Contenedorllamada } from "./styled";
+import Menu from "../menu/principal";
+
+const Audiollamada = () => {
+    return(
+        <>
+        <Menu/>
+        <Contenedorllamada>llamada de audio</Contenedorllamada></>
+    )
+}
+export default Audiollamada;
