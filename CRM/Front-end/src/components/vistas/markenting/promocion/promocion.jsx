376ca8fa59a61dfa4a5fe634920b4f0b74204b50
Elias Negrete <eliasnegrete777@gmail.com>
2023-09-27 15:23:54 -0500

    todo perfecto

--- CRM/Front-end/src/components/vistas/markenting/promocion/promocion.jsx
+++ CRM/Front-end/src/components/vistas/markenting/promocion/promocion.jsx
@@ -1,12 +1,48 @@
 import React from "react";
-import { Contenedor } from "./styled";
+import { BodyCard, Container, ContainerCard, FooterCard, HeaderCard, Card, Title, BoxPromotion } from "./styled";
 import Menu from "../../menu/principal";
 
 const Promocion = () => {
     return(
         <>
         <Menu/>
-        <Contenedor>promocion</Contenedor></>
+            <Container>
+                <BoxPromotion>
+                    <Title><h1>planes de promociónddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</h1></Title>
+                    <ContainerCard>
+                        <Card>
+                            <HeaderCard></HeaderCard>
+                            <BodyCard>
+                                
+                            </BodyCard>
+                            <FooterCard></FooterCard>
+                        </Card>
+                        <Card>
+                            <HeaderCard></HeaderCard>
+                            <BodyCard>
+                                
+                            </BodyCard>
+                            <FooterCard></FooterCard>
+                        </Card>
+                        <Card>
+                            <HeaderCard></HeaderCard>
+                            <BodyCard>
+
+                            </BodyCard>
+                            <FooterCard></FooterCard>
+                        </Card>
+                        <Card>
+                            <HeaderCard></HeaderCard>
+                            <BodyCard>
+
+                            </BodyCard>
+                            <FooterCard></FooterCard>
+                        </Card>
+                    </ContainerCard>
+                </BoxPromotion>
+            </Container>
+            </>
+            
     )
 }
 export default Promocion
--- CRM/Front-end/src/components/vistas/markenting/promocion/styled.jsx
+++ CRM/Front-end/src/components/vistas/markenting/promocion/styled.jsx
@@ -1,7 +1,54 @@
 import styled from "styled-components";
 
-export const Contenedor = styled.div`
+export const Container = styled.div`
     width: 100%;
     height: 90%;
-    background-color: green;
+`
+
+export const BoxPromotion = styled.div`
+    width: 100%;
+    height: 90%;
+    background-color: #48242b;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+`
+export const Title = styled.div`
+    width: 100%;
+    height: 60px;
+    background-color: #021bfc;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 25px;
+`
+
+export const ContainerCard = styled.div`
+    width: 80%;
+    height: 80%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    background-color: aquamarine;
+`
+export const Card = styled.div`
+    width: 20%;
+    height: 100%;
+    background-color: #00000073;
+`
+export const HeaderCard = styled.div`
+    width: 100%;
+    height: 15%;
+    background-color: #00f;
+`
+export const BodyCard = styled.div`
+    width: 100%;
+    height: 70%;
+    background-color: #ff0000;
+`
+export const FooterCard = styled.div`
+    width: 100%;
+    height: 15%;
+    background-color: #2086df;
 `
