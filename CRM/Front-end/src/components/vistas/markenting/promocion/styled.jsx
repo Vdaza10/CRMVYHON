4fd7bd59ebad4faad866dc2e8838186d41669139
Elias Negrete <eliasnegrete777@gmail.com>
2023-09-28 17:53:26 -0500

    membresia

--- CRM/Front-end/src/components/vistas/markenting/promocion/styled.jsx
+++ CRM/Front-end/src/components/vistas/markenting/promocion/styled.jsx
@@ -14,11 +14,11 @@ export const BoxPromotion = styled.div`
     align-items: center;
 `
 
-export const ContainerTitleText = styled.div`
+export const ContainerTitle = styled.div`
     width: 100%;
-    height: 18%;
+    height: 10%;
     display: flex;
-    flex-direction: column;
+    align-items: center;
 `
 
 export const Title = styled.div`
@@ -27,74 +27,171 @@ export const Title = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    font-size: 20px;
+    font-size: 15px;
 `
-export const SmallText = styled.div`
-    width: 100%;
-    height: 50%;
+export const HeaderTable = styled.div`
+    width: 53%;
+    height: 3%;
+    border: solid 2px #0000008d;
     display: flex;
     justify-content: center;
+    &.hijo{
+        width: 32.9%;
+        height: 100%;
+        background-color: #141111b3;
+    }
 `
 
 export const ContainerCard = styled.div`
-    width: 80%;
-    height: 80%;
+    width: 53%;
+    height: 90%;
     display: flex;
     flex-direction: row;
-    justify-content: space-evenly;
+    justify-content: center;
+    align-items: center;
+    border: solid 2px #0000008d;
 `
-export const Card = styled.div`
-    width: 20%;
-    height: 100%;
+export const Image = styled.img`
+    width: 50%;
+    height: 75%;
 `
-
-export const HeaderCard = styled.div`
+export const ContainerImg = styled.div`
+    background-color: #ffffff;
     width: 100%;
-    height: 17%;
-    font-size: 25px;
+    height: 65%;
     display: flex;
     align-items: center;
     justify-content: center;
-    background-color: #447ad7;
+`
+export const LettersHeaderCard = styled.div`
+color: #000;
+    width: 100%;
+    height: 35%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+`
+export const Icono = styled.div`
+    width: 5%;
+    height: 5%;
+` 
+export const Card = styled.div`
+    width: 33.5%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const HeaderCard = styled.div`
+    width: 90%;
+    height: 40%;
+    font-size: 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
     color: #ffffff;
+    border-bottom: 1px solid #00000088;
 `
 
 export const BodyCard = styled.div`
     width: 100%;
-    height: 78%;
+    height: 50%;
     display: flex;
     flex-direction: column;
-    justify-content: space-evenly;
+    align-items: center;
+
 `
 
 export const RowCard = styled.div`
-    width: 100%;
-    height: 19.5%;
+    width: 80%;
+    height: 20%;
     font-size: 20px;
     display: flex;
     align-items: center;
     justify-content: center;
+    border-bottom: 1px solid #0000007b;
+    &.containerButton{
+        margin-top: 5px;
+        display: flex;
+        align-items: end;
+        justify-content: center;
+        border-bottom: none;
+    } 
+
+`
+export const Card2 = styled.div`
+    width: 33%;
+    height: 100%;
+    border: solid 2px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const HeaderCard2 = styled.div`
+    width: 90%;
+    height: 45%;
+    font-size: 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-bottom: 1px solid #00000064;
+    color: #ffffff;
+
+
+`
+
+export const BodyCard2 = styled.div`
+    width: 100%;
+    height: 45%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const RowCard2 = styled.div`
+    width: 80%;
+    height: 20%;
+    font-size: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-bottom: 1px solid #00000084;
+    &.card3{
+        height: 9%;
+    }
+    &.card6{
+        height: 9%;
+    }
 `
 export const RowCard1 = styled.div`
-    width: 100%;
-    height: 19.5%;
+    width: 80%;
+    height: 20%;
     font-size: 20px;
     display: flex;
     align-items: center;
-    background-color: #dfeef8;
     justify-content: center;
+    border-bottom: 1px solid #0000006f;
 `
 
 export const FooterCard = styled.div`
     width: 100%;
-    height: 7%;
+    height: 10%;
     display: flex;
     align-items: center;
     justify-content: center;
 `
 export const Button = styled.button`
     width: 40%;
-    height: 80%;
+    height: 65%;
     color: #fff;
-    border: solid 1px #0000006a;
+    background-color: #ffc506;
+    border-radius: 4px;
+    border: solid 2px #00000029;
+
+
 `
