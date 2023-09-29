55bcf3402a1aee60a084de558aac53e23c9e6811
JasiFlorez <jasilff2002@gmail.com>
2023-09-29 14:50:44 -0500

    jasil

--- CRM/Front-end/src/components/vistas/markenting/markentingp.jsx
+++ CRM/Front-end/src/components/vistas/markenting/markentingp.jsx
@@ -1,68 +1,12 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
 import jwt_decode from "jwt-decode";
-import { BoxModulos, MenuContainer } from "./styled";
+import { Contenido, Enlace, PanelControl, Modulo, Submodulo } from "./styled";
 
-const Campaña = () => {
-  const [modalAbierta, setModalAbierta] = useState(false);
+const Campaign = () => {
   const [loading, setLoading] = useState(true);
-  const [acordeonAbierto, setAcordeonAbierto] = useState([
-    false,
-    false,
-    false,
-    false,
-  ]);
-
-  let navigate = useNavigate();
-
-  // Definir los módulos y submódulos
-  const modulos = [
-    {
-      nombre: "Módulo 1",
-      submodulos: [
-        { nombre: "Submódulo 1.1", ruta: "/submodulo1-1" },
-        { nombre: "Submódulo 1.2", ruta: "/submodulo1-2" },
-      ],
-    },
-    {
-      nombre: "Módulo 2",
-      submodulos: [
-        { nombre: "Submódulo 2.1", ruta: "/submodulo2-1" },
-        { nombre: "Submódulo 2.2", ruta: "/submodulo2-2" },
-      ],
-    },
-    {
-      nombre: "Módulo 3",
-      submodulos: [
-        { nombre: "Submódulo 3.1", ruta: "/submodulo3-1" },
-        { nombre: "Submódulo 3.2", ruta: "/submodulo3-2" },
-      ],
-    },
-    {
-      nombre: "Módulo 4",
-      submodulos: [
-        { nombre: "Submódulo 4.1", ruta: "/submodulo4-1" },
-        { nombre: "Submódulo 4.2", ruta: "/submodulo4-2" },
-      ],
-    },
-    {
-      nombre: "Módulo 4",
-      submodulos: [
-        { nombre: "Submódulo 4.1", ruta: "/submodulo4-1" },
-        { nombre: "Submódulo 4.2", ruta: "/submodulo4-2" },
-      ],
-    },
-    {
-      nombre: "Módulo 4",
-      submodulos: [
-        { nombre: "Submódulo 4.1", ruta: "/submodulo4-1" },
-        { nombre: "Submódulo 4.2", ruta: "/submodulo4-2" },
-      ],
-    },
-    // Agrega más módulos y submódulos según sea necesario
-  ];
+  const [moduloAbierto, setModuloAbierto] = useState(null); // Estado para el módulo abierto
+  const navigate = useNavigate();
 
   useEffect(() => {
     const userToken = localStorage.getItem("user");
@@ -80,40 +24,71 @@ const Campaña = () => {
     }
   }, [navigate]);
 
-  const toggleAcordeon = (index) => {
-    const nuevosEstados = [...acordeonAbierto];
-    nuevosEstados[index] = !nuevosEstados[index];
-    setAcordeonAbierto(nuevosEstados);
+  const toggleModulo = (modulo) => {
+    if (moduloAbierto === modulo) {
+      // Si el mismo módulo está abierto, ciérralo
+      setModuloAbierto(null);
+    } else {
+      // Si otro módulo estaba abierto, ciérralo antes de abrir el nuevo
+      setModuloAbierto(modulo);
+    }
   };
 
   return (
     <>
       {loading ? (
-        <>
-          <h1>cargando.....</h1>
-        </>
+        <h1>Cargando...</h1>
       ) : (
-          <MenuContainer>
-            {modulos.map((modulo, index) => (
-              <BoxModulos key={index}>
-                <button onClick={() => toggleAcordeon(index)}>
-                  {modulo.nombre}
-                </button>
-                {acordeonAbierto[index] && (
-                  <ul>
-                    {modulo.submodulos.map((submodulo, subIndex) => (
-                      <li key={subIndex}>
-                        <a href={submodulo.ruta}>{submodulo.nombre}</a>
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </BoxModulos>
-            ))}
-          </MenuContainer>
+        <>
+          <PanelControl>
+            <h2>Panel de Control</h2>
+            {/* Módulo 1 */}
+            <Modulo
+              onClick={() => toggleModulo("moduloCampañas")}
+              className={moduloAbierto === "moduloCampañas" ? "abierto" : ""}
+            >
+              Módulo de Campañas
+            </Modulo>
+            {moduloAbierto === "moduloCampañas" && (
+              <div>
+                <Submodulo>
+                  <Enlace href="/submodulo1-1">Email Campaign</Enlace>
+                </Submodulo>
+                <Submodulo>
+                  <Enlace href="/submodulo1-2">Audio Call</Enlace>
+                </Submodulo>
+                <Submodulo>
+                  <Enlace href="/submodulo1-3">SMS Campaign</Enlace>
+                </Submodulo>
+              </div>
+            )}
+
+            {/* Módulo 2 */}
+            <Modulo
+              onClick={() => toggleModulo("moduloOtro")}
+              className={moduloAbierto === "moduloOtro" ? "abierto" : ""}
+            >
+              Otro Módulo
+            </Modulo>
+            {moduloAbierto === "moduloOtro" && (
+              <div>
+                <Submodulo>
+                  <Enlace href="/submodulo2-1">Submódulo 2.1</Enlace>
+                </Submodulo>
+                <Submodulo>
+                  <Enlace href="/submodulo2-2">Submódulo 2.2</Enlace>
+                </Submodulo>
+              </div>
+            )}
+
+          </PanelControl>
+          <Contenido>
+            <h1>Contenido de la Vista Campaign</h1>
+          </Contenido>
+        </>
       )}
     </>
   );
 };
 
-export default Campaña;
+export default Campaign;
--- CRM/Front-end/src/components/vistas/markenting/styled.jsx
+++ CRM/Front-end/src/components/vistas/markenting/styled.jsx
@@ -1,13 +1,33 @@
 import styled from "styled-components";
 
-export const MenuContainer = styled.div`
-  height: 100%;
+export const PanelControl = styled.div`
+  background-color: #f0f0f0;
   width: 15%;
-  background-color: gray;
+  flex-direction: column;
+  height: 100%;
+  overflow-y: auto;
 `;
 
-export const BoxModulos=styled.div`
-    background-color: #e97b7b;
-    width: 100%;
-    height: 10%;
-`
+export const Contenido = styled.div`
+  margin-left: 260px;
+  padding: 20px;
+`;
+
+export const Enlace = styled.a`
+  display: block;
+  margin-bottom: 10px;
+  text-decoration: none;
+  color: #333;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const Modulo = styled.div`
+  font-size: 20px;
+  cursor: pointer; /* Hacer que el módulo sea clicable */
+`;
+
+export const Submodulo = styled.div`
+background-color: red;
+`;
