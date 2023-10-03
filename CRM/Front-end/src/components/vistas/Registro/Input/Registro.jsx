504df42bf735ce95e766d0e5cd8ccba3805605f2
Elias Negrete <eliasnegrete777@gmail.com>
2023-10-03 11:38:57 -0500

    validacion de campo de registro correo

--- CRM/Front-end/src/components/vistas/Registro/Input/Registro.jsx
+++ CRM/Front-end/src/components/vistas/Registro/Input/Registro.jsx
@@ -1,35 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Contenedor,
+  Contenedor2,
+  Titulo,
+  Contenedor1,
   Input,
   Boton,
-  Seleccionar,
-  Contenedor1,
   Checkbox,
-  Titulo,
-  TextoImput,
   TextoCheckbox,
+  Seleccionar,
+  TextoImput,
   ContaienrBoton,
-  Contenedor2,
-} from "./styled";
-import { useState } from "react";
+} from "./styled"; // Importa tus estilos desde el archivo "styled"
 import Axios from "axios";
 import validator from "validator";
 import VentanaModal1 from "../../../modales/mensajeRegistro";
 import UserExiste from "../../../modales/mensajeRegistroYaExiste";
-import swal from "sweetalert"
-import "../../../../App.css"
+import swal from "sweetalert";
+import "../../../../App.css";
 
 const Registrarse = () => {
   const [password, setPassword] = useState("");
   const [nombre, setNombre] = useState("");
   const [empresa, setEmpresa] = useState("");
   const [email, setEmail] = useState("");
-
   const [correoExistente, setCorreoExistente] = useState(false);
-
   const [estadoModal1, cambiarEstadoModal1] = useState(false);
   const [estadoModal4, cambiarEstadoModal4] = useState(false);
+  const [emailValid, setEmailValid] = useState(true); // Estado para rastrear la validez del correo
+  const [buttonClicked, setButtonClicked] = useState(false); // Estado para rastrear si se hizo clic en el botón
+  const [mostrarAlertaCorreo, setMostrarAlertaCorreo] = useState(false); // Estado para mostrar el mensaje de alerta en el correo
 
   const VentanaModal = () => {
     cambiarEstadoModal1(!estadoModal1);
@@ -38,18 +38,24 @@ const Registrarse = () => {
   const VentanaModal4 = () => {
     cambiarEstadoModal4(!estadoModal4);
   };
+
   const Validacion = (e) => {
     let emai = e.target.value;
 
     if (validator.isEmail(emai)) {
+      setEmailValid(true); // El correo es válido
       setEmail(emai);
+      setMostrarAlertaCorreo(false); // Ocultar el mensaje de alerta
     } else {
-      // setEmail('enter valid Email');
+      setEmailValid(false); // El correo no es válido
+      setMostrarAlertaCorreo(true); // Mostrar el mensaje de alerta
     }
   };
+
   const Registro = (ev) => {
     ev.preventDefault();
-    if (email && password && nombre && empresa) {
+    setButtonClicked(true); // Se hizo clic en el botón
+    if (emailValid && password && nombre && empresa) {
       Axios.post(`${process.env.REACT_APP_URL_BACKEND}/users`, {
         correo: email,
         contraseña: password,
@@ -70,8 +76,8 @@ const Registrarse = () => {
         });
     } else {
       swal({
-        title: "ingresa informacion en los campo",
-        text: "porfavor revisa que todos los datos esten bien",
+        title: "Ingresa información en los campos",
+        text: "Por favor, revisa que todos los datos estén bien",
         icon: "error",
       });
     }
@@ -93,14 +99,14 @@ const Registrarse = () => {
                 ¿What's your name?
               </h3>
             </TextoImput>
-            <Input 
-            onKeyPress={(event) => {
-              const inputValue = event.key;
-              const regex = /[a-zA-Z ]/; // Expresión regular que permite letras y números
-              if (!regex.test(inputValue)) {
-                event.preventDefault(); // Evita que se ingrese el carácter si no cumple con la expresión regular
-              }
-            }}
+            <Input
+              onKeyPress={(event) => {
+                const inputValue = event.key;
+                const regex = /[a-zA-Z ]/;
+                if (!regex.test(inputValue)) {
+                  event.preventDefault();
+                }
+              }}
               onChange={(event) => {
                 setNombre(event.target.value);
               }}
@@ -112,17 +118,17 @@ const Registrarse = () => {
           <Contenedor1>
             <TextoImput>
               <h3 style={{ margin: "0", fontWeight: "500" }}>
-                ¿What company you work for ?
+                ¿What company you work for?
               </h3>
             </TextoImput>
             <Input
-            onKeyPress={(event) => {
-              const inputValue = event.key;
-              const regex = /[a-zA-Z0-9& ]/;
-              if (!regex.test(inputValue)) {
-                event.preventDefault(); 
-              }
-            }}
+              onKeyPress={(event) => {
+                const inputValue = event.key;
+                const regex = /[a-zA-Z0-9& ]/;
+                if (!regex.test(inputValue)) {
+                  event.preventDefault();
+                }
+              }}
               onChange={(event) => {
                 setEmpresa(event.target.value);
               }}
@@ -138,19 +144,23 @@ const Registrarse = () => {
               </h3>
             </TextoImput>
             <Input
-
-            onKeyPress={(event) => {
-              const inputValue = event.key;
-              const regex = /[_.@a-zA-Z0-9]/;
-              if (!regex.test(inputValue)) {
-                event.preventDefault(); 
-              }
-            }}
-
+              onKeyPress={(event) => {
+                const inputValue = event.key;
+                const regex = /[_.@a-zA-Z0-9]/;
+                if (!regex.test(inputValue)) {
+                  event.preventDefault();
+                }
+              }}
               onChange={(e) => Validacion(e)}
               type="email"
               required
+              className={!emailValid && buttonClicked ? "invalid-email" : ""}
             ></Input>
+            {mostrarAlertaCorreo && (
+              <div style={{color:"red"}} className="alert alert-danger" role="alert">
+                ingrese todos los carracteres 
+              </div>
+            )}
             {correoExistente && (
               <span
                 style={{
@@ -166,7 +176,7 @@ const Registrarse = () => {
           <Contenedor1>
             <TextoImput>
               <h3 style={{ margin: "0", fontWeight: "500" }}>
-              Create a password for your account
+                Create a password for your account
               </h3>
             </TextoImput>
             <Input
@@ -179,16 +189,18 @@ const Registrarse = () => {
           </Contenedor1>
 
           <Checkbox>
-            <Seleccionar type="checkbox" required ></Seleccionar>
+            <Seleccionar type="checkbox" required></Seleccionar>
             <TextoCheckbox>
-              I agree with the terms of use of the software and know{" "}
+              I agree with the terms of use of the software and know
               <br />
               the privacy policy
             </TextoCheckbox>
           </Checkbox>
 
           <ContaienrBoton>
-            <Boton type="submit" onClick={Registro}>Create Account</Boton>
+            <Boton type="submit" onClick={Registro}>
+              Create Account
+            </Boton>
           </ContaienrBoton>
         </Contenedor2>
       </Contenedor>
