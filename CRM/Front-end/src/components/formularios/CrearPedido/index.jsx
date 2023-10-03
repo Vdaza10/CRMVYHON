e7f716d55ac6d44c0afd8abd6fb44286cc8beb1a
oscar gomez <Ogomez@ulearning.com.co>
2023-10-03 08:52:14 -0500

    error solucionado

--- CRM/Front-end/src/components/formularios/CrearPedido/index.jsx
+++ CRM/Front-end/src/components/formularios/CrearPedido/index.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { Button, FormGroup, FormularioContainer, Input, Label } from "./style.jsx";
+import React, { useState } from 'react';
+import { Button, FormGroup, FormularioContainer, Input, Label } from './style.jsx';
+import axios from 'axios';
 
-const FormularioPedido = () => {
+const FormularioPedido = ({ onTaskCreated }) => {
   const [cliente, setCliente] = useState('');
   const [monto, setMonto] = useState('');
 
@@ -18,25 +18,26 @@ const FormularioPedido = () => {
     event.preventDefault();
 
     try {
-      // Obtener la fecha actual en el formato deseado (año/mes/día)
       const fecha = new Date().toISOString().slice(0, 10);
-
-      // Enviar los datos a la base de datos usando la API
-      await axios.post('URL_DE_TU_API/crearPedidos', {
+      const nuevaTarea = {
         cliente: cliente,
         monto: monto,
         fecha: fecha,
-      });
+      };
 
-      // Restablecer los campos del formulario después de enviar los datos
+      // Enviar la nueva tarea a la base de datos mediante una solicitud POST
+      const response = await axios.post("http://localhost:3005/pedidos", nuevaTarea);
+      const createdTask = response.data;
+
+      // Llamar a la función onTaskCreated y pasarle la nueva tarea creada
+      onTaskCreated(createdTask);
+
+      // Limpiar los campos después de enviar la tarea
       setCliente('');
       setMonto('');
-
-      // Aquí puedes manejar cualquier lógica adicional después de enviar los datos
-      console.log('Datos enviados correctamente.');
+      console.log('Tarea creada correctamente.');
     } catch (error) {
-      console.error('Error al enviar los datos:', error);
-      // Aquí puedes manejar errores de envío, como mostrar un mensaje de error al usuario
+      console.error('Error al crear la tarea:', error);
     }
   };
 
