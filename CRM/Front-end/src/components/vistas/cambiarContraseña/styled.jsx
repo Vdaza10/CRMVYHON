23c125a2c18c0dfd01da9e2c526db6fd4639e666
Elias Negrete <eliasnegrete777@gmail.com>
2023-10-04 16:33:18 -0500

    elia termino

--- CRM/Front-end/src/components/vistas/cambiarContraseña/styled.jsx
+++ CRM/Front-end/src/components/vistas/cambiarContraseña/styled.jsx
@@ -1,14 +1,21 @@
 import styled from "styled-components";
-import "../../../App.css"
-
+import "../../../App.css";
 
 export const Fondo = styled.div`
-background: var( --color-fondo);
-background: -moz-linear-gradient(180deg, var( --color-fondoo) 0%, rgba(238,238,238,1) 100%);
-background: -webkit-linear-gradient(180deg,  var(--color-fondoo) 0%, rgba(238,238,238,1) 100%);
-background: linear-gradient(180deg, var(--color-fondoo) 0%, rgba(238,238,238,1) 100%);
-filter: progid:DXImageTransform.Microsoft.gradient(startColorstr= var(--color-background),endColorstr="#eeeeee",GradientType=1);
-  height: 100vh;
+  background: var(--color-fondo);
+  background: -moz-linear-gradient(
+    180deg,
+    var(--color-fondoo) 0%,
+    #3f3f3f 100%
+  );
+  background: -webkit-linear-gradient(
+    180deg,
+    var(--color-fondoo) 0%,
+    #535252 100%
+  );
+  background: linear-gradient(180deg, var(--color-fondoo) 0%, #5a5b5c 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr= var(--color-background),endColorstr="#2e2e2e",GradientType=1);
+  height: 100%;
   width: 100%;
   display: flex;
   align-items: center;
@@ -17,7 +24,6 @@ filter: progid:DXImageTransform.Microsoft.gradient(startColorstr= var(--color-ba
 
 export const Contenedor = styled.div`
   border-radius: 3px solid;
-  background-color: #ffff;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -40,33 +46,33 @@ export const Titulo = styled.h1`
 `;
 
 export const Parrafo = styled.p`
-    font-size: 22px;
-    font-weight: 200;
-    font-style: italic;
-    display: flex;
-    margin-left: 5%;
-    margin-right:5%;
-    /* justify-content: center; */
-    align-items: center;
-    text-align: center;
-`
+  font-size: 22px;
+  font-style: italic;
+  display: flex;
+  color: white;
+  margin-left: 5%;
+  margin-right: 5%;
+  /* justify-content: center; */
+  align-items: center;
+  text-align: center;
+`;
 
 export const ConteinerBonton = styled.div`
   height: 25%;
   width: 100%;
   display: flex;
   justify-content: center;
+  justify-content: space-evenly;
+  align-items: center;
   /* display: flex; */
-`
+`;
 
 export const Boton1 = styled.button`
   background-color: var(--color-button);
-  color: #0c0b0b;
-  width: 35%;
-  height: 30%;
+  color: #f6f4f4;
+  width: 125px;
+  height: 40px;
   border: none;
-  margin-right: 20px;
-  margin-top: 30px;
   font-size: 15px;
   cursor: pointer;
   &:hover {
@@ -81,31 +87,32 @@ export const Boton1 = styled.button`
 `;
 
 export const Boton2 = styled.button`
-    background-color: white;
-    color: var(--color-boton2) ;
-    width: 100%;
-    height: 100%;
-    font-size: 15px;
-    margin-top: 30px;
-    border: solid 1px gray;
-    cursor: pointer;
+  background-color: var(--color-button);
+  color: #f6f4f4;
+  width: 125px;
+  height: 40px;
+  border: none;
+  font-size: 15px;
+  cursor: pointer;
 
-    &:hover {
-        background-color: var(--color-buttonHover);
-        color: var(--color-boton2Hover);
-    }
+  &:hover {
+    background-color: var(--color-buttonHover);
+    color: var(--color-boton2Hover);
+  }
 
-    &:focus {
-        outline: none;
-        box-shadow: 0 0 3px var(--color-buttonFocus);}
-`
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 3px var(--color-buttonFocus);
+  }
+`;
 
 export const Input = styled.input`
-  width: 80%;
-  height:9%;
+  width: 280px;
+  height: 35px;
   border: none;
   margin-bottom: 20px;
-  font-size: 16.5px;
+  font-size: 20px;
+  color: #ffffff;
   padding-left: 8px;
   background-color: var(--color-input);
 `;
