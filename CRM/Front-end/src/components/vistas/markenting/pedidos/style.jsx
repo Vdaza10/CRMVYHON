56a9c1228ee569194addb58a18cc13bc10c23444
oscar gomez <Ogomez@ulearning.com.co>
2023-10-06 16:53:16 -0500

    style orders

--- CRM/Front-end/src/components/vistas/markenting/pedidos/style.jsx
+++ CRM/Front-end/src/components/vistas/markenting/pedidos/style.jsx
@@ -6,23 +6,32 @@ export const Div = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-  background-color: #ffffff; /* Cambiado a verde oscuro */
+  background-color: #ffffff;
 `;
 
 export const ButtonCont = styled.div`
   width: 90%;
   height: 10%;
+  background-color: #ffffff;
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 
+export const State = styled.div`
+  height: 5%;
+  width: 90%;
+  display: flex;
+  justify-content: center;
+  justify-content: space-around;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
 export const Container = styled.div`
   width: 90%;
   height: 80%;
-  margin-top: 20px;
   display: flex;
-  justify-content: center;
   justify-content: space-between;
 `;
 
@@ -30,9 +39,12 @@ export const Column = styled.div`
   width: 24%;
   max-height: 100%;
   border-radius: 10px;
+  border: solid 1px;
   display: flex;
   gap: 10px;
+  padding-top: 20px;
   flex-direction: column;
+  align-items: center;
   overflow-y: auto;
   ::-webkit-scrollbar {
     overflow: hidden;
@@ -41,23 +53,52 @@ export const Column = styled.div`
 
 export const H2 = styled.h2`
   font-size: 25px;
-  border-bottom: solid #9e9c9c;
+  color: #6AB7BD;
+  border-bottom: solid #6AB7BD;
   text-align: center;
   margin: 5px;
 `;
 
 export const Task = styled.div`
-  background-color: #4b6e5b; /* Cambiado a un tono más claro de verde */
-  border: 1px solid #3a5240; /* Cambiado a un tono más oscuro de verde */
+  height: 20%;
+  width: 90%;
+  background-color: #f2f2f2;
+  border: 1px solid #ddd;
   border-radius: 5px;
-  padding: 15px;
   cursor: pointer;
+  transition: background-color 0.3s ease, transform 0.3s ease;
+
   &:hover {
-    background-color: #5c8366; /* Cambiado a un tono más claro de verde en hover */
+    background-color: #ffffff;
+    transform: scale(1.05);
   }
-  &:active {
-    transform: translateY(-4px);
-  }
+`;
+
+export const TitleClient = styled.div`
+  height: 20%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const DataOrders = styled.div`
+  height: 80%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-evenly;
+`;
+
+export const Data = styled.div`
+  width: 100%;
+  text-align: center;
+`;
+
+export const Button = styled.button`
+
 `;
 
 export const DeleteButton = styled.button`
@@ -77,10 +118,11 @@ export const DeleteButton = styled.button`
 
 export const Form = styled.form`
   width: 300px;
-  background-color: #2e4739; /* Cambiado a verde oscuro */
+  background-color: #ffffff;
   border-radius: 10px;
   padding: 20px;
   box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  margin-top: 20px;
 `;
 
 export const FormGroup = styled.div`
@@ -91,18 +133,19 @@ export const FormLabel = styled.label`
   display: block;
   font-weight: bold;
   margin-bottom: 5px;
+  color: #6AB7BD;
 `;
 
 export const FormInput = styled.input`
   width: 100%;
   padding: 10px;
-  border: 1px solid #3a5240; /* Cambiado a un tono más oscuro de verde */
+  border: 1px solid #ddd;
   border-radius: 5px;
 `;
 
 export const FormButton = styled.button`
   width: 100%;
-  background-color: #4CAF50;
+  background-color: #6AB7BD;
   color: white;
   border: none;
   border-radius: 5px;
@@ -111,6 +154,7 @@ export const FormButton = styled.button`
   transition: background-color 0.3s ease;
 
   &:hover {
-    background-color: #45a049;
+    background-color: #4E8B8D;
   }
 `;
+
